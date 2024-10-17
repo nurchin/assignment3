361f0fe67b0078d80d35dd400a18af7b9138c915
nurcin <nurchin.14@gmail.com>
2024-10-17 15:32:08 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a value:" );
+            int number = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+
+            if (number <= 1)
+            {
+                Console.WriteLine("This number is neither composite nor prime.");
+                return;
+            }
+
+            for (int i = 2; i < number; i++) { 
+            
+            if (number%i==0) {
+
+                    Console.WriteLine("It is a composite number.");
+                    isPrime = false;
+                    break;
+                }    
+
+            }
+
+
+            if (isPrime) {
+                Console.WriteLine("It is a prime number."); 
+            }
+
+
+
+
+        }
+    }
+}
